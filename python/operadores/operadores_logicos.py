37e40fe8cb83ab4bc9c41c56ab8dcd1c31b57862
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-08-26 13:57:53 -0300

    feat: trabalhando com operadores logicos em python

---
+++ python/operadores/operadores_logicos.py
@@ -0,0 +1,12 @@
+saldo = 100;
+limite = 10;
+saque =20;
+depositos = [];
+
+print(saldo >=saque and saque <= limite); # espressao e
+print(saldo >=saque or saque <= limite); # espressao ou 
+print(not saldo > limite); # expressao de negacao
+print(not depositos);
+print(not "");
+print(not "testando");
+print((saque >limite) and (limite >= saque));
