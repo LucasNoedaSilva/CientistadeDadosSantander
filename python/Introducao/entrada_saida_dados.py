8ec4fb3c5e1c441a65652e480b073766283cd469
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-08-26 00:30:37 -0300

    featch: trabalhando com entrada e saida de dados em python

---
+++ python/Introducao/entrada_saida_dados.py
@@ -0,0 +1,9 @@
+valor =int(input("informe seu valor"));
+print(type(valor));
+valor = int(valor);
+print(valor+2);
+
+nome = input("Digie seu nome: ");
+print("Seu nome é: ", nome);
+print("seu nome é:", nome, end="... \n");
+print("seu nome é:", nome, sep="#")
