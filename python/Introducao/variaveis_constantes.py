ba17f223b803f5a40a25b593e2af015dc456f6f4
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-08-26 00:27:12 -0300

    featch: trabalhando com variaveis e constantes em python

---
+++ python/Introducao/variaveis_constantes.py
@@ -0,0 +1,12 @@
+idade =25;
+print("Minha idade é:",idade);
+idade= 26;
+print("Mas ano que vem vai ser", idade);
+
+#variavel constante
+NOME= "Lucas";
+print(NOME);
+limite_saque_diario = 1000;
+saque = 100;
+saldo = limite_saque_diario - saque;
+print(saldo);
