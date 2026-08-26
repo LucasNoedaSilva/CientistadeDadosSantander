f25236fa146be913c7f6db26b5fc83427c272adf
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-08-26 00:29:34 -0300

    featch: trabalhando com a conversao de tipo em python

---
+++ python/Introducao/convertendo_tipos.py
@@ -0,0 +1,16 @@
+preco = 10.60;
+print(preco);
+preco = int(preco);
+print(preco);
+print(preco /2);
+print(preco //2);
+
+
+#de string para numerico
+
+dinheiro ="20";
+dinheiro = int(dinheiro);
+print(dinheiro);
+dinheiro = float(dinheiro);
+print(dinheiro);
+print(type(dinheiro));
