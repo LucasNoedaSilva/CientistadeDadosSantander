895375143417a54893236dd7723e523d721d064b
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-08-26 23:23:21 -0300

    feat: trabalhando com estruturas condicionais em python

---
+++ python/estrutura/Estruturas_condicionais.py
@@ -0,0 +1,12 @@
+idade = int(input("digite sua idade"));
+
+if(idade >= 18):
+    print("Pode tirar a carta já");
+elif(idade>=16 and idade <=18):
+    print("Pode tirar a carta com supervisao dos pais");
+else:
+    print("Nao pode tirar")
+
+status = "Pode tirar a carta" if idade > 18 else "Nao pode";
+print(status)
+
