ff1803d9ce979d8cf879bbfeecd25718f5e1a488
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-09-01 13:01:51 -0300

    feat: conhecendo alguns metodos de string em python

---
+++ python/funcoes/Metodos_uteis.py
@@ -0,0 +1,11 @@
+nome = "   luCaS Noe ";
+
+print(nome.upper());
+print(nome.lower());
+print(nome.title());
+print(nome.strip());
+print(nome.lstrip());
+print(nome.rstrip());
+print(nome.center(20,"#"));
+print(" " .join(nome));
+
